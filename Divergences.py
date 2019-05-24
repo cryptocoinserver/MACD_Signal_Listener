6170fc343ff798b62fe8766cb23cdada7f415aa8
raulMrello <raul.martin.rello@gmail.com>
2019-05-24 19:22:56 +0200

    Developing search function

--- Divergences.py
+++ Divergences.py
@@ -90,7 +90,7 @@ class Divergences():
     self.__logger.info('Created!')
     self.__zigzag = ZIGZAG_Signal_Listener(level)
 
-  def searchDivergences(self, df, zigzag_cfg = dict(), level=logging.WARN):    
+  def searchDivergences(self, df, zigzag_cfg = dict(), level=logging.WARN, exitAt='end'):    
     """Builds from dataframe df, next indicators: MACD, RSI, Stochastic with default
        parameters. Then builds a zigzag indicator.
 
@@ -101,24 +101,29 @@ class Divergences():
       minbars -- Min. number of bars per flip to avoid discarding (default 12)
       zigzag_cfg -- Dictionary with zigzag configuration (default None)
       level -- logging level (default WARN)
+      exitAt -- Label for exit at debugging phase
     """
     
     # clear events
     self.__events.clear()
 
     #builds zigzag
-    _minbars   = zigzag_cfg['minbars'] if 'minbars' in zigzag_cfg.keys() else 12 , 
-    _bb_period = zigzag_cfg['bb_period'] if 'bb_period' in zigzag_cfg.keys() else 20, 
-    _bb_dev    = zigzag_cfg['bb_dev'] if 'bb_dev' in zigzag_cfg.keys() else 2.0,
-    _bb_sma    = zigzag_cfg['bb_sma'] if 'bb_sma' in zigzag_cfg.keys() else [100],
-    _nan_value = zigzag_cfg['nan_value'] if 'nan_value' in zigzag_cfg.keys() else 0.0, 
+    _minbars   = zigzag_cfg['minbars'] if 'minbars' in zigzag_cfg.keys() else 12  
+    _bb_period = zigzag_cfg['bb_period'] if 'bb_period' in zigzag_cfg.keys() else 2 
+    _bb_dev    = zigzag_cfg['bb_dev'] if 'bb_dev' in zigzag_cfg.keys() else 2.0
+    _bb_sma    = zigzag_cfg['bb_sma'] if 'bb_sma' in zigzag_cfg.keys() else [100]
+    _nan_value = zigzag_cfg['nan_value'] if 'nan_value' in zigzag_cfg.keys() else 0.0 
+    _zlevel    = zigzag_cfg['level'] if 'level ' in zigzag_cfg.keys() else logging.WARN
     _df, _x, _y, _evt_zigzag =  self.__zigzag.ZIGZAG( df, 
                                                       minbars   = _minbars,
                                                       bb_period = _bb_period,
                                                       bb_dev    = _bb_dev,
                                                       bb_sma    = _bb_sma,
-                                                      nan_value = _nan_value
-                                                      level     = level)
+                                                      nan_value = _nan_value,
+                                                      level     = _zlevel)
+
+    if exitAt == 'zigzag-calculation':
+      return _df.copy()
 
     # build MACD_main, RSI and Stochastic
     _df['MACD_main'], _df['MACD_signal'], _df['MACD_hist'] = talib.MACD(_df.CLOSE, fastperiod=12, slowperiod=26, signalperiod=9)
@@ -129,68 +134,71 @@ class Divergences():
     _df.dropna(inplace=True)
     _df.reset_index(drop=True, inplace=True)
 
+    if exitAt == 'oscillator-built':
+      return _df.copy()
+
     # executes divergence localization process:
     # 1. Set a default trend: requires 3 max and 3 min points
     # 1a.If max increasing or min increasing -> Bullish trend
     # 1b.If max decreasing or min decreasing -> Bearish trend
     # 1c.Else discard.
 
-    def search(row, df, nan_value):
+    def search(row, df, nan_value, logger, exitAt):
+      log = 'row [{}]: '.format(row.name)
+      if exitAt == 'row-count-processing':
+        logger.debug(log)
+        return
+
       # skip rows where no zzpoints
-      if row.ZIGZAG == nan_value: return
-      # get last 6 zigzag points
-      zzpoints = df.ZIGZAG[(df.index <= row.index) & (df.ZIGZAG != nan_value)]
-      # discard if no 6 points
-      if zzpoints.shape[0] < 6: return
-      # get indexes of 2 last min and max points:
-      if zzpoints[-1] > zzpoints[-2]:
-        max1_idx = zzpoints[-3].index
-        max0_idx = zzpoints[-1].index
-        min1_idx = zzpoints[-4].index
-        min0_idx = zzpoints[-2].index
+      if row.ZIGZAG == nan_value: 
+        log += 'error-zigzag-isnan'
+
       else:
-        # get indexes of last 2 min and max points
-        max1_idx = zzpoints[-4].index
-        max0_idx = zzpoints[-2].index
-        min1_idx = zzpoints[-3].index
-        min0_idx = zzpoints[-1].index
+        # get last 6 zigzag points
+        zzpoints = df.ZIGZAG[(df.index <= row.name) & (df.ZIGZAG != nan_value)]
+        # discard if no 6 points
+        if zzpoints.shape[0] < 6: 
+           log += 'error-zzpoints-count={} '.format(zzpoints.shape[0])
+        
+        else:
+          log += 'zzpoints={} '.format(zzpoints[-6:])
+          if exitAt == 'zzpoints-processing':
+            logger.debug(log)
+            return
 
-      # check if is bullish trend
-      if ((zzpoints[-1] >= zzpoints[-3] and zzpoints[-3] >= zzpoints[-5]) or (zzpoints[-2] >= zzpoints[-4] and zzpoints[-4] >= zzpoints[-6])):
+          # get indexes of 2 last min and max points:
+          if zzpoints[-1] > zzpoints[-2]:
+            log += 'last is MAX '
+            max1_idx = zzpoints.index[-3]
+            max0_idx = zzpoints.index[-1]
+            min1_idx = zzpoints.index[-4]
+            min0_idx = zzpoints.index[-2]
+            log += 'max0={}, min0={}, max1={}, min1={} '.format(max0_idx, min0_idx, max1_idx, min1_idx)
+            #TODO<------------
 
-      # check if is bearish trend
-      elif ((zzpoints[-1] <= zzpoints[-3] and zzpoints[-3] <= zzpoints[-5]) or (zzpoints[-2] <= zzpoints[-4] and zzpoints[-4] <= zzpoints[-6])):
-      
-      # is an undefined trend, then discard calculation
-      else: return 
+          else:
+            log += 'last is MIN '
+            # get indexes of last 2 min and max points
+            max1_idx = zzpoints.index[-4]
+            max0_idx = zzpoints.index[-2]
+            min1_idx = zzpoints.index[-3]
+            min0_idx = zzpoints.index[-1]
+            log += 'max0={}, min0={}, max1={}, min1={} '.format(max0_idx, min0_idx, max1_idx, min1_idx)
+            #TODO<------------        
+
+          # check if is bullish trend
+          if ((zzpoints[-1] >= zzpoints[-3] and zzpoints[-3] >= zzpoints[-5]) or (zzpoints[-2] >= zzpoints[-4] and zzpoints[-4] >= zzpoints[-6])):
+            log += 'bullish-trend '
+          # check if is bearish trend
+          elif ((zzpoints[-1] <= zzpoints[-3] and zzpoints[-3] <= zzpoints[-5]) or (zzpoints[-2] <= zzpoints[-4] and zzpoints[-4] <= zzpoints[-6])):
+            log += 'bearish-trend '
+          # is an undefined trend, then discard calculation
+          else: 
+            log += 'error-no-trend'
+      logger.debug(log)
       #---end-of-search-function
 
-    _df.apply(lambda x: self.__search(x, _df, _nan_value), axis=1)
-
-    # 1. Load first 2 consecutive max in zigzag indicator
-    # 2. If increasing then check if regular bearish divergence on any oscillator
-    # 1b. If decreasing then check if hidden bullish divergence on any oscillator
-
-    # Initially no actions are in progress, record first high and low values creating an ActionCtrl object
-    action = ActionCtrl(
-              high= _df['HIGH'][0], #max(_df['OPEN'][0], _df['CLOSE'][0]), 
-              low = _df['LOW'][0], #min(_df['OPEN'][0], _df['CLOSE'][0]), 
-              idx = _df.iloc[0].name, 
-              delta= minbars,
-              level=level)
-
-    _df['ZIGZAG'] = nan_value
-    _df['ACTION'] = 'no-action'
-    _df['ACTION'] = _df.apply(lambda x: action.zigzag(x, _df), axis=1)
-
-    # fills last element as pending
-    if _df.ZIGZAG.iloc[-1] == nan_value:
-      _df.at[_df.index[-1],'ZIGZAG'] =  action.last_high if _df.ACTION.iloc[-1] == 'high' else action.last_low
-      _df.at[_df.index[-1],'ACTION'] =  _df.ACTION.iloc[-1] + '-in-progress'
-    
-    self.__df = _df
-    self.__action = action
-    return self.__df, self.__action.x, self.__action.y, self.__action.events
-
-  def getDataFrame(self):
+    # execute search
+    self.__df = _df.apply(lambda x: search(x, _df, _nan_value, self.__logger, exitAt), axis=1)
     return self.__df
+
